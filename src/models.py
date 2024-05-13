d74ae7ca7e4efc8ef113c5570dd5bea553babb0e
TomasArancibia <101353494+TomasArancibia@users.noreply.github.com>
2024-05-13 19:52:01 +0000

    modeles ig

--- src/models.py
+++ src/models.py
@@ -7,26 +7,140 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(20), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(80), nullable=False)
+    name = Column(String(20), nullable=False)
+    lastname = Column(String(20))
+    age = Column(Integer, nullable=False)
 
-    def to_dict(self):
-        return {}
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "username": self.username,
+            "name": self.name,
+            "lastname": self.lastname,
+            "age": self.age
+        }
+
+
+class Follower(Base):
+    __tablename__ = "follower"
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
+        }
+
+
+class Feed(Base):
+    __tablename__ = "feed"
+    id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type
+        }
+
+
+class Location(Base):
+    __tablename__ = "location"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    country = Column(String, nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+            "city": self.city,
+            "country": self.country
+        }
+
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    description = Column(String(400), nullable=False)
+    likecount = Column(Integer)
+    source_url = Column(String, nullable=False)
+    date = Column(String, nullable=False)
+    location = Column(Integer, ForeignKey("location.id"), nullable=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    feed_id  = Column(Integer, ForeignKey('feed.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "like_count": self.likecount,
+            "location": self.location,
+            "date": self.date,
+            "source_url": self.source_url
+        }
+
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    text = Column(String(400), nullable=True)
+    author_id  = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id  = Column(Integer, ForeignKey("post.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "author_id": self.author_id,
+            "post_id": self.post_id
+        }
+
+
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id
+        }
+
+
+class Like(Base):
+    __tablename__ = "like"
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("post.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        }
 
 ## Draw from SQLAlchemy base
 try:
